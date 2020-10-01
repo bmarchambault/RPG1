0c89317cc2b70f19e965c55fcdf400bb7e06cb7f
Bobbie Archambault <bmarchambault@gmail.com>
2020-10-01 16:27:31 -0500

    implemented random number method and defend

--- src/consoleAdventureGame/AdventureGame.java
+++ src/consoleAdventureGame/AdventureGame.java
@@ -6,18 +6,23 @@ public abstract class AdventureGame {
 
     Input userInput = new Input();
     protected String herosName;
-    protected int herosHealth;
-    protected int enemysHealth;
+    protected int herosHealth = 100;
+    protected int enemysHealth = 100;
     protected int attack = 5;
     protected int defend = 3;
     protected int potion = 15;
     protected int remainingHeath;
     protected boolean endGame = false;
+    protected int randomNumber;
 
 //    public abstract void attack();
-//    public void defend(){};
+    public void defend(){};
 
     public abstract void remainingHealth ();
     public abstract void attacked();
     public abstract String getHerosName();
+
+    public int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 }
